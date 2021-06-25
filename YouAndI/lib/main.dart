6dead5417d0deea2dd1a3beaedb793a70e1c2ef9
Michelle Adeline <michelleadelinee@gmail.com>
2021-06-25 18:04:01 +0800

    log in linked

--- YouAndI/lib/main.dart
+++ YouAndI/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:test/constants.dart';
+import 'package:test/homepage.dart';
 import 'package:test/welcomepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -10,9 +12,9 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   final Future<FirebaseApp> _fpApp = Firebase.initializeApp();
-
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: Constants.BG_BASE,
@@ -20,10 +22,18 @@ class MyApp extends StatelessWidget {
       home: FutureBuilder(
         future: _fpApp,
         builder: (context, snapshot) {
+
           if (snapshot.hasError) {
             print('You have an error! ${snapshot.error.toString()}');
             return Text('Something went wrong!');
           } else if (snapshot.hasData) {
+
+            User? user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              WidgetsBinding.instance!
+                  .addPostFrameCallback((_) => Navigator.of(context, rootNavigator: true).pushReplacement(MaterialPageRoute(builder: (context) => HomePage())));
+            }
+
             return WelcomePage();
           } else {
             return Center(
